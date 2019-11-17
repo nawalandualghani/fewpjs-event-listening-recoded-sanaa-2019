8a57973487351376136d86f8a51176f4ad9c36c9
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 08:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
+function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
+}
